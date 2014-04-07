278116c9eabe21537ee1da1c745b0fa86a45c383
Asao <kamei@workspot.jp>
2014-04-07 19:44:53 +0900

    about to create schme for post.

---
+++ app/database/migrations/2014_04_07_093640_create_post_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+        Schema::create( 'post', function($table) {
+            /** @var Blueprint $table */
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
